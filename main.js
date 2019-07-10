53c26538b8bd889ce331073027d2c1c7c58129a3
ycy <568082704@qq.com>
2019-07-10 16:30:08 +0800

    一个关于闭包的问题

--- main.js
+++ main.js
@@ -14,29 +14,46 @@ function dongwu() {
 }
 dongwu(a)
 //阶乘
-var x = parseInt(window.prompt('input'));
+var x = parseInt(window.prompt("input"))
 function jiechen(n) {
-    if (n == 0) {
-        console.log("1");
-    } else if (n > 0) {
-        var a = 1;
-        for (i = 1; i <= n; i++) {
-            a *= i;
-        }
-        console.log(a);
+  if (n == 0) {
+    console.log("1")
+  } else if (n > 0) {
+    var a = 1
+    for (i = 1; i <= n; i++) {
+      a *= i
     }
+    console.log(a)
+  }
 }
 //斐波那契数列
-var x = parseInt(window.prompt('input'));
-        function fei(n) {
-            var f = 1,
-                s = 1,
-                t;
-            for (i = 0; i < n - 2; i++) {
-                t = f + s;
-                f = s;
-                s = t;
-            }
-            document.write(t);
-        }
-        fei(x)
+var x = parseInt(window.prompt("input"))
+function fei(n) {
+  var f = 1,
+    s = 1,
+    t
+  for (i = 0; i < n - 2; i++) {
+    t = f + s
+    f = s
+    s = t
+  }
+  document.write(t)
+}
+fei(x)
+
+//一个关于闭包的问题
+function test() {
+  var arr = []
+  for (var i = 0; i < 10; i++) {
+    arr[i] = function() {
+      console.log(i + " ")
+    }
+  }
+  return arr
+}
+test()
+document.write(i)
+document.write(myArr)
+for (var j = 0; j < 10; j++) {
+  myArr[j]()
+}
