2b4b36465a171d4fa4481ed3b5c11b29d0450ae6
ycy <568082704@qq.com>
2019-07-17 16:37:39 +0800

    解决斐波那契数列前两项输出错误的问题

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-var num = 123123.3456789
-document.write(num.toFixed(3))
-// 可以四舍五入，科学计数法，小数点后3位
-//实现输入动物名，就答应响应的动物叫声
-var a = String(window.prompt("input"))
-function dongwu() {
-  if (a == "老虎") {
-    document.write("吼")
-  } else if (a == "狗") {
-    document.write("旺")
-  } else if (a == "猪") {
-    document.write("咕噜")
-  }
-}
-dongwu(a)
-//阶乘
-var x = parseInt(window.prompt("input"))
-function jiechen(n) {
-  if (n == 0) {
-    console.log("1")
-  } else if (n > 0) {
-    var a = 1
-    for (i = 1; i <= n; i++) {
-      a *= i
-    }
-    console.log(a)
-  }
-}
+// var num = 123123.3456789
+// document.write(num.toFixed(3))
+// // 可以四舍五入，科学计数法，小数点后3位
+// //实现输入动物名，就答应响应的动物叫声
+// var a = String(window.prompt("input"))
+// function dongwu() {
+//   if (a == "老虎") {
+//     document.write("吼")
+//   } else if (a == "狗") {
+//     document.write("旺")
+//   } else if (a == "猪") {
+//     document.write("咕噜")
+//   }
+// }
+// dongwu(a)
+// //阶乘
+// var x = parseInt(window.prompt("input"))
+// function jiechen(n) {
+//   if (n == 0) {
+//     console.log("1")
+//   } else if (n > 0) {
+//     var a = 1
+//     for (i = 1; i <= n; i++) {
+//       a *= i
+//     }
+//     console.log(a)
+//   }
+// }
 //斐波那契数列
-var x = parseInt(window.prompt("input"))
+var x = parseInt(window.prompt("input"))//有修改空间
 function fei(n) {
+  if (n==1||n==2){
+    document.write(1)
+  }
   var f = 1,
     s = 1,
     t
@@ -41,39 +44,40 @@ function fei(n) {
 }
 fei(x)
 
-//一个关于闭包的问题
-function test() {
-  var arr = []
-  for (var i = 0; i < 10; i++) {
-    arr[i] = function() {
-      console.log(i + " ")
-    }
-  }
-  return arr
-}
-test()
-document.write(i)
-document.write(myArr)
-for (var j = 0; j < 10; j++) {
-  myArr[j]()
-}
+// //一个关于闭包的问题
+// function test() {
+//   var arr = []
+//   for (var i = 0; i < 10; i++) {
+//     arr[i] = function() {
+//       console.log(i + " ")
+//     }
+//   }
+//   return arr
+// }
+// test()
+// document.write(i)
+// document.write(myArr)
+// for (var j = 0; j < 10; j++) {
+//   myArr[j]()
+// }
 
-//计算2的n次幂
-    var n = parseInt(window.prompt('imput'));
-    if (n == 0) {
-        document.write("1");
-    } else {
-        var a = 1;
-        for (i = 0; i < n; i++) {
-            a *= 2;
-        }
-        document.write(a);
+// //计算2的n次幂
+//     var n = parseInt(window.prompt('imput'));
+//     if (n == 0) {
+//         document.write("1");
+//     } else {
+//         var a = 1;
+//         for (i = 0; i < n; i++) {
+//             a *= 2;
+//         }
+//         document.write(a);
 
-    }
-    //反向输出三位数
-    var n = parseInt(window.prompt('imput'));
-    var a, b, c;
-    a = n % 10;
-    b = parseInt(n % 100 / 10);
-    c = parseInt(n / 100);
-    document.write(a * 100 + b * 10 + c);
+//     }
+//     //反向输出三位数
+//     var n = parseInt(window.prompt('imput'));
+//     var a, b, c;
+//     a = n % 10;
+//     b = parseInt(n % 100 / 10);
+//     c = parseInt(n / 100);
+//     document.write(a * 100 + b * 10 + c);
+
